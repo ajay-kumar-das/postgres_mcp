ffa92dfb7e095e25cd9c079b137a8b0a52f15a7b
ajay-kumar-das <ajaydas.6080@gmail.com>
2025-07-25 14:25:23 +0530

    token based db authentication

--- mcp/src/main/kotlin/com/kasafal/mcp/config/JacksonConfig.kt
+++ mcp/src/main/kotlin/com/kasafal/mcp/config/JacksonConfig.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -22,7 +23,7 @@ class JacksonConfig {
             .serializationInclusion(JsonInclude.Include.NON_NULL)
             .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .modules(KotlinModule.Builder().build())
+            .modules(KotlinModule.Builder().build(), JavaTimeModule())
             .build()
     }
 }
